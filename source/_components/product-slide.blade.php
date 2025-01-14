1a247cb1df2023728216aa58a3d81ae0e0ee91f7
Kevin Chandra <kacol.bot@gmail.com>
2025-01-14 11:38:17 +0700

    reusable slide

--- source/_components/product-slide.blade.php
+++ source/_components/product-slide.blade.php
@@ -1,55 +1,21 @@
-<div class="container mx-auto flex h-full flex-col md:flex-row md:gap-x-4 font-gotham-rounded">
-    <div class="w-full md:w-6/12">
-        <div class="flex h-full flex-col items-center justify-center">
-            <img src="/assets/images/babygo/diaper-bag/aeon-diaper-bag.png" alt="aeon diaper bag"
-                 class="mx-auto h-full max-h-60 w-auto md:max-h-[540px]">
+<div class="swiper-slide" data-hash="{{ \Illuminate\Support\Str::slug($title) }}">
+    <div class="container mx-auto flex h-full flex-col md:flex-row md:gap-x-4 font-gotham-rounded">
+        <div class="w-full md:w-6/12">
+            <div class="flex h-full flex-col items-center justify-center">
+                <img src="{{ $image }}" alt="{{ \Illuminate\Support\Str::slug($title) }}"
+                     class="mx-auto h-full max-h-60 w-auto md:max-h-[540px]">
+            </div>
         </div>
-    </div>
-    <div class="w-full md:w-6/12">
-        <div class="flex h-full items-center justify-center p-4">
-            <div class="mt-4 w-full rounded-2xl p-5 text-center md:mt-8"
-                 style="background-color:#daefe3;max-width: 625px">
-                <div class="-mt-10  md:-mt-12">
-                    <h3 class="mb-4 text-3xl font-bold md:mb-10 md:text-5xl"
-                        style="color: #069247; text-shadow: -3px -3px 2px #fff, -3px 3px 2px #fff, 3px -3px 2px #fff, 3px 3px 2px #fff, -1px -1px 0 #fff, -1px 1px 0 #fff, 1px -1px 0 #fff, 1px 1px 0 #fff;">
-                        Aeon Diaper Bag
-                    </h3>
-                    <div class="inline-block rounded-full text-xl font-bold leading-none text-white px-2.5 py-0.5 md:px-3.5 md:py-0 md:text-3xl"
-                         style="background-color:#6dac33;">
-                        Dimensi Tas
-                    </div>
-                    <div class="my-1 text-lg md:my-5 md:text-3xl" style="color: #455058">
-                        P x L x T : 25 x 14 x 38 (cm)
-                    </div>
-                    <div class="inline-block rounded-full text-xl font-bold leading-none text-white px-2.5 py-0.5 md:px-3.5 md:py-0 md:text-3xl"
-                         style="background-color:#6dac33;">
-                        Tersedia dalam Warna
-                    </div>
-                    <div class="flex flex-row justify-center gap-x-4 mt-2.5 md:gap-x-9">
-                        <div class="flex items-center justify-center rounded-full text-base font-medium text-white size-16 md:size-20 md:text-lg"
-                             style="background-color:#29465d;">
-                            Biru
-                        </div>
-                        <div class="flex items-center justify-center rounded-full text-base font-medium text-white size-16 md:size-20 md:text-lg"
-                             style="background-color:#292925;">
-                            Hitam
-                        </div>
-                        <div class="flex items-center justify-center rounded-full text-base font-medium text-white size-16 md:size-20 md:text-lg"
-                             style="background-color:#614e58;">
-                            Coklat
-                        </div>
-                    </div>
-                    <div class="mt-5 flex flex-row md:gap-x-20">
-                        <div>
-                            <img src="/assets/images/babygo/diaper-bag/bonus-waterproof.png"
-                                 alt=""
-                                 class="h-auto w-full max-w-full">
-                        </div>
-                        <div>
-                            <img src="/assets/images/babygo/diaper-bag/model-aeon-diaper-bag.jpeg"
-                                 alt=""
-                                 class="h-auto w-full max-w-full">
-                        </div>
+        <div class="w-full md:w-6/12">
+            <div class="flex h-full items-center justify-center p-4">
+                <div class="mt-4 w-full rounded-2xl p-5 text-center md:mt-8"
+                     style="background-color:#daefe3;max-width: 625px">
+                    <div class="-mt-10  md:-mt-12">
+                        <h3 class="mb-4 text-3xl font-bold md:mb-10 md:text-5xl"
+                            style="color: #069247; text-shadow: -3px -3px 2px #fff, -3px 3px 2px #fff, 3px -3px 2px #fff, 3px 3px 2px #fff, -1px -1px 0 #fff, -1px 1px 0 #fff, 1px -1px 0 #fff, 1px 1px 0 #fff;">
+                            {{ $title }}
+                        </h3>
+                        {!! $slot !!}
                     </div>
                 </div>
             </div>
