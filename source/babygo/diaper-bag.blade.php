67e724e24776d1792928125b78acd6de7aa9e77f
Kevin Chandra <kacol.bot@gmail.com>
2025-01-14 14:29:54 +0700

    fix

--- source/babygo/diaper-bag.blade.php
+++ source/babygo/diaper-bag.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
     <div class="h-screen snap-y snap-proximity overflow-auto scroll-smooth">
-        <section title="Intro" class="min-h-screen snap-none md:h-screen md:snap-start">
+        <section title="Intro" class="md:min-h-screen snap-none md:h-screen md:snap-start">
             @include('_partials.babygo.intro')
         </section>
-        <section title="Komitmen" class="min-h-screen snap-none md:h-screen md:snap-start">
+        <section title="Komitmen" class="md:min-h-screen snap-none md:h-screen md:snap-start">
             @include('_partials.babygo.komitmen')
         </section>
-        <section title="Products" class="min-h-screen snap-none md:h-screen md:snap-start">
+        <section title="Products" class="md:min-h-screen snap-none md:h-screen md:snap-start">
             @include('_partials.babygo.products')
         </section>
-        <section title="Contact" class="min-h-screen snap-none md:h-screen md:snap-start">
+        <section title="Contact" class="md:min-h-screen snap-none h-screen md:snap-start">
             @include('_partials.babygo.contact')
         </section>
     </div>
