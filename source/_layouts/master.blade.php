81c113d0ab3743c4a6119f671f9b13c5b3c57491
Kevin Chandra <kacol.bot@gmail.com>
2025-03-21 10:41:29 +0700

    chore: gtm and pixel

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -17,8 +17,37 @@
     <link rel="stylesheet" href="../{{ mix('/css/main.css', 'assets/build') }}">
     @stack('after_styles')
     <base href="{{ $page->baseUrl }}">
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1231606828132277');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1231606828132277&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5GP2PNR5');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body class="antialiased font-gotham-rounded text-body">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5GP2PNR5"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 @yield('content')
 <div class="fixed z-30 bottom-4 right-4">
     <a href="https://wa.me/{{ $page->whatsapp_no }}" target="_blank">
